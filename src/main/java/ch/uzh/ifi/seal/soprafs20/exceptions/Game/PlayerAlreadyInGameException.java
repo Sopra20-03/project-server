6cd88ebc981f99ae2965183a48a1111ba831b0e8
Hannah <hannah.rohe@hispeed.ch>
2020-04-07 17:44:27 +0200

    FS20G03-53 #comment added exception for when player is already in game

--- src/main/java/ch/uzh/ifi/seal/soprafs20/exceptions/Game/PlayerAlreadyInGameException.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/exceptions/Game/PlayerAlreadyInGameException.java
@@ -1,5 +1,9 @@
 package ch.uzh.ifi.seal.soprafs20.exceptions.Game;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Player already in Game")
 public class PlayerAlreadyInGameException extends RuntimeException {
     public PlayerAlreadyInGameException(String message) { super(" : Player with"+message+"is already in the game"); }
 }
