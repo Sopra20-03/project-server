f59c700af0db537a9ca2434a641e133be6cb29fe
Hannah <hannah.rohe@hispeed.ch>
2020-04-09 20:54:07 +0200

    FS20G03-57 #comment created class WordCard (added a column for each word.. maybe a list of words is better??)

---
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/entity/WordCard.java
@@ -0,0 +1,97 @@
+package ch.uzh.ifi.seal.soprafs20.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "T_WordCards")
+@SequenceGenerator(name="wordCardSeq", initialValue=1, allocationSize=100)
+public class WordCard implements Serializable {
+
+    private static final long serialVersionUID = 2L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "wordCardSeq")
+    private long wordCardId;
+
+    @Column
+    private String selectedWord;
+
+    //TODO: column for each word or list of words??
+    @Column(nullable = false)
+    private String word1;
+
+    @Column(nullable = false)
+    private String word2;
+
+    @Column(nullable = false)
+    private String word3;
+
+    @Column(nullable = false)
+    private String word4;
+
+    @Column(nullable = false)
+    private String word5;
+
+    public WordCard() {
+        //empty constructor
+    }
+
+
+    public long getWordCardId() {
+        return wordCardId;
+    }
+
+    public void setWordCardId(long wordCardId) {
+        this.wordCardId = wordCardId;
+    }
+
+    public String getSelectedWord() {
+        return selectedWord;
+    }
+
+    public void setSelectedWord(String selectedWord) {
+        this.selectedWord = selectedWord;
+    }
+
+    public String getWord1() {
+        return word1;
+    }
+
+    public void setWord1(String word1) {
+        this.word1 = word1;
+    }
+
+    public String getWord2() {
+        return word2;
+    }
+
+    public void setWord2(String word2) {
+        this.word2 = word2;
+    }
+
+    public String getWord3() {
+        return word3;
+    }
+
+    public void setWord3(String word3) {
+        this.word3 = word3;
+    }
+
+    public String getWord4() {
+        return word4;
+    }
+
+    public void setWord4(String word4) {
+        this.word4 = word4;
+    }
+
+    public String getWord5() {
+        return word5;
+    }
+
+    public void setWord5(String word5) {
+        this.word5 = word5;
+    }
+
+}
