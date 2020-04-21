9fc0a5a3a7867285a428fddc5eb9632a7aceca85
TomWartm <55591955+TomWartm@users.noreply.github.com>
2020-04-21 11:25:17 +0200

    add a check if all clues are submitted before enter a new guess

---
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/exceptions/Clue/NotEnoughCluesException.java
@@ -0,0 +1,5 @@
+package ch.uzh.ifi.seal.soprafs20.exceptions.Clue;
+
+public class NotEnoughCluesException extends RuntimeException{
+    public NotEnoughCluesException(String message){super(message);}
+}
