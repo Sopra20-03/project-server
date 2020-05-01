2c9ec84efa5721f0adad0609aee9468c57c0befc
TomWartm <55591955+TomWartm@users.noreply.github.com>
2020-05-01 11:06:11 +0200

    change status code

--- src/main/java/ch/uzh/ifi/seal/soprafs20/exceptions/User/UserAlreadyExistsException.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/exceptions/User/UserAlreadyExistsException.java
@@ -4,7 +4,7 @@ package ch.uzh.ifi.seal.soprafs20.exceptions.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+@ResponseStatus(value = HttpStatus.CONFLICT)
 public class UserAlreadyExistsException extends RuntimeException {
     public UserAlreadyExistsException(String message) {
         super(" : User with "+message+" already exists.");
